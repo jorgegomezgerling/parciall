f7a004c66ae31b1937922de48a0c2001fb3f99f3
jorgegomezgerling <95316729+jorgegomezgerling@users.noreply.github.com>
2023-06-13 20:38:21 -0300

    Add files via upload

---
+++ colas.py
@@ -0,0 +1,34 @@
+
+class Cola():
+
+    def __init__(self):
+        self.__elementos = []
+
+    def arrive(self, value):
+        self.__elementos.append(value)
+
+    def atention(self):
+        if self.size() > 0:
+            return self.__elementos.pop(0)
+
+    def size(self):
+        return len(self.__elementos)
+
+    def on_front(self):
+        if self.size() > 0:
+            return self.__elementos[0]
+
+    def move_to_end(self):
+        if self.size() > 0:
+            aux = self.atention()
+            self.arrive(aux)
+            return aux
+    
+    def barrido(self):
+        i = 0
+        tamanio = self.size()
+        while i < tamanio:
+            dato = self.move_to_end()
+            print(dato)
+            i += 1
+    
